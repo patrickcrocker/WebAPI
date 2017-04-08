36347fcddccb9c42ab890bb4fdda079b86edf8f1
Cornelius Mendoza <cmendoza@pivotal.io>
2017-04-07 23:25:32 -0500

    Updates for Config Server

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Steeltoe.Extensions.Configuration;
 using Steeltoe.CloudFoundry.Connector.PostgreSql.EFCore;
+using Steeltoe.CloudFoundry.Connector.OAuth;
 using WebAPI.Models;
 
 namespace WebAPI
@@ -22,6 +23,8 @@ namespace WebAPI
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables()
 
+                .AddConfigServer(env)
+
                 .AddCloudFoundry();
             Configuration = builder.Build();
         }
@@ -31,11 +34,21 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            // Optional: Adds IConfigurationRoot as a service and also configures IOption<ConfigServerClientSettingsOptions> 
+            // Performs:
+            //      services.AddOptions();
+            //      services.Configure<ConfigServerClientSettingsOptions>(config);
+            //      services.AddSingleton<IConfigurationRoot>(config);
+            services.AddConfigServer(Configuration);
+
+            //// Configure and Add IOptions<OAuthServiceOptions> to the container
+            //services.AddOAuthServiceOptions(Configuration);
+
             // Add framework services.
             services.AddMvc();
 
             // Add Context and use Postgres as provider ... provider will be configured from VCAP_ info
-
             services.AddDbContext<TreasureContext>(options => options.UseNpgsql(Configuration));
         }
 
